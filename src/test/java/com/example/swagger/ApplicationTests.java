ebc768493196fd1bb336a03268be94c2fc779bd6
Christoph Dreis <christoph.dreis@innogames.com>
2022-08-04 07:51:28 +0200

    Add test

--- src/test/java/com/example/swagger/ApplicationTests.java
+++ src/test/java/com/example/swagger/ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.swagger;
 
+import io.swagger.v3.core.converter.AnnotatedType;
+import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.converter.ResolvedSchema;
+import io.swagger.v3.oas.models.media.Schema;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void schemaExample() {
+		ResolvedSchema resolvedSchema = ModelConverters.getInstance()
+				.resolveAsResolvedSchema(new AnnotatedType(TestData.class));
+
+		Schema<?> testDataSchema = resolvedSchema.referencedSchemas.get("TestData");
+		Schema<?> idSchema = testDataSchema.getProperties().get("id");
+		assertThat(idSchema.getExampleSetFlag()).isTrue();
 	}
 
 }
